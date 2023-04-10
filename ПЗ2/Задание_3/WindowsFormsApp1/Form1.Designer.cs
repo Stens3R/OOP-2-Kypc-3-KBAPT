622b985fb46a6e4730439d1220bef188b6c74c66
MedzhitovRamazan <79780216790@mail.ru>
2023-04-10 16:30:16 +0300

    Задание 3 полностью

---
+++ ПЗ2/Задание_3/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_num2 = new System.Windows.Forms.TextBox();
+            this.textBox_num1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(253, 43);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 0;
+            this.button.Text = "Вычислить";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1 значение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2 значение";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "НОД (Евклид):";
+            // 
+            // textBox_num2
+            // 
+            this.textBox_num2.Location = new System.Drawing.Point(86, 62);
+            this.textBox_num2.Name = "textBox_num2";
+            this.textBox_num2.Size = new System.Drawing.Size(100, 20);
+            this.textBox_num2.TabIndex = 4;
+            // 
+            // textBox_num1
+            // 
+            this.textBox_num1.Location = new System.Drawing.Point(86, 29);
+            this.textBox_num1.Name = "textBox_num1";
+            this.textBox_num1.Size = new System.Drawing.Size(100, 20);
+            this.textBox_num1.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox_num1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox_num2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(450, 426);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Задание 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(250, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "НОД (Штейн):";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_num2;
+        private System.Windows.Forms.TextBox textBox_num1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ПЗ2/Задание_3/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int num_1, num_2;
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (!Int32.TryParse(textBox_num1.Text, out num_1))
+            {
+                MessageBox.Show("Неверно задано значение!, Введите число cнова");
+            }
+            if (!Int32.TryParse(textBox_num2.Text, out num_2))
+            {
+                MessageBox.Show("Неверно задано значение!, Введите число cнова");
+            }
+
+            int result;
+            long time;
+            (result, time) = NOD(num_1, num_2);
+
+            label3.Text += "\nРезультат:" + result.ToString() +
+                "\nВремя выполнения(в тиках):" + time.ToString() + "\n";
+
+
+
+            (result, time) = FindGCDStein(num_1, num_2);
+
+            label4.Text += "\nРезультат:" + result.ToString() +
+                "\nВремя выполнения(в тиках):" + time.ToString() + "\n";
+        }
+
+
+
+        public static (int nod, long time) NOD(int a, int b)//метод Евклида
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            
+            if (a == 0) return (b, timer.ElapsedTicks);
+            while (b != 0)
+            {
+                if (a > b)
+                {
+                    a -= b;
+                }
+                else
+                {
+                    b -= a;
+                }
+            }
+            return (a, timer.ElapsedTicks);
+        }
+
+        static public (int nod, long time) FindGCDStein(int u, int v)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            int k;
+            // Step 1.
+            // gcd(0, v) = v, because everything divides zero, 
+            // and v is the largest number that divides v. 
+            // Similarly, gcd(u, 0) = u. gcd(0, 0) is not typically 
+            // defined, but it is convenient to set gcd(0, 0) = 0.
+            if (u == 0 || v == 0)
+                return (u | v, timer.ElapsedTicks);
+            // Step 2.
+            // if u and v are both even, then gcd(u, v) = 2•gcd(u/2, v/2), 
+            // because 2 is a common divisor. 
+            for (k = 0; ((u | v) & 1) == 0; ++k)
+            {
+                u >>= 1;
+                v >>= 1;
+            }
+            // Step 3.
+            // if u is even and v is odd, then gcd(u, v) = gcd(u/2, v), 
+            // because 2 is not a common divisor. 
+            // Similarly, if u is odd and v is even, 
+            // then gcd(u, v) = gcd(u, v/2). 
+
+            while ((u & 1) == 0)
+                u >>= 1;
+            // Step 4.
+            // if u and v are both odd, and u ≥ v, 
+            // then gcd(u, v) = gcd((u − v)/2, v). 
+            // If both are odd and u < v, then gcd(u, v) = gcd((v − u)/2, u). 
+            // These are combinations of one step of the simple 
+            // Euclidean algorithm, 
+            // which uses subtraction at each step, and an application 
+            // of step 3 above. 
+            // The division by 2 results in an integer because the 
+            // difference of two odd numbers is even.
+            do
+            {
+                while ((v & 1) == 0)  // Loop x
+                    v >>= 1;
+                // Now u and v are both odd, so diff(u, v) is even.
+                //   Let u = min(u, v), v = diff(u, v)/2. 
+                if (u < v)
+                {
+                    v -= u;
+                }
+                else
+                {
+                    int diff = u - v;
+                    u = v;
+                    v = diff;
+                }
+                v >>= 1;
+                // Step 5.
+                // Repeat steps 3–4 until u = v, or (one more step) 
+                // until u = 0.
+                // In either case, the result is (2^k) * v, where k is 
+                // the number of common factors of 2 found in step 2. 
+            } while (v != 0);
+            u <<= k;
+            return (u, timer.ElapsedTicks);
+        }
+    }
+}
