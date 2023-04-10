f2c25ff73025000ab097c7b3059529263324aa58
MedzhitovRamazan <79780216790@mail.ru>
2023-04-10 14:26:23 +0300

    Передалано задание 1 на win forms(.Net framework)

---
+++ ПЗ2/Задание_1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApp1;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NOD_2806and345_return23()
+        {
+            int a = 2806;
+            int b = 345;
+            int expected = 23;
+
+            int actual = Form1.NOD(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- ПЗ2/Задание_1/WindowsFormsApp1/Form1.cs
+++ ПЗ2/Задание_1/WindowsFormsApp1/Form1.cs
@@ -1,4 +1,14 @@
-namespace Задание_1
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
@@ -7,7 +17,7 @@ namespace Задание_1
             InitializeComponent();
         }
 
-        int num_1, num_2, num_3, num_4, num_5;
+        int num_1, num_2;
         public static int NOD(int a, int b)//метод Евклида
         {
             if (a == 0) return b;
